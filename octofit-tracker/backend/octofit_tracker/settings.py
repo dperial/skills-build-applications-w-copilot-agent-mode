3b6d590700353de1be8027241302f471395cc5d2
pblerial <dperial44@gmail.com>
2025-09-03 11:55:10 +0000

    Ajout de champs de groupe et de permissions à la classe User, mise à jour de la configuration des URL pour gérer les environnements de développement et ajustement des hôtes autorisés dans les paramètres.

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -1,3 +1,5 @@
+# Utilisation du modèle utilisateur personnalisé
+AUTH_USER_MODEL = 'octofit_tracker.User'
 """
 Django settings for octofit_tracker project.
 
@@ -25,7 +27,11 @@ SECRET_KEY = 'django-insecure-sxmyvu8f#&)#v*i+)b^fezin&b=um-p_t0u6ic7wba43l)0qu2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
